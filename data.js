c50d240f9857b53e05345aa6985c6ba7c542da16
Nir Yariv <niryariv@gmail.com>
2012-12-12 16:52:42 +0200

    initial draft for local council transparency score

---
+++ data.js
@@ -0,0 +1,7 @@
+
+var data = [
+	{ city: "Tel Aviv", coords: [32.06, 34.78], score: 83.5 },
+	{ city: "Jerusalem", coords: [31.78, 35.21], score: 91 },
+	{ city: "Haifa", coords: [32.81, 34.98], score: 86 },
+	{ city: "Beer Sheva", coords: [31.24, 34.79], score: 80 },
+]
