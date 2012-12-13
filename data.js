26edb488e9456c42846cee651d1a464852e0cb56
Nir Yariv <niryariv@gmail.com>
2012-12-13 20:22:35 +0200

    full screen map

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 
 var data = [
-	{ id: "telaviv",	name: "תל אביב",	coords: [32.06, 34.78], score: 83.5 },
+	{ id: "telaviv",	name: "תל אביב",		coords: [32.06, 34.78], score: 83.5 },
 	{ id: "haifa", 		name: "חיפה",		coords: [32.81, 34.98], score: 86 	},
 	{ id: "jerusalem",	name: "ירושלים",		coords: [31.78, 35.21], score: 91 	},
 	{ id: "beersheva",	name: "באר שבע",	coords: [31.24, 34.79], score: 80 	},
