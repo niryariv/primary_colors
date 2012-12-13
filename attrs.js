cb3dbd6fb81bbf1c25b176c388b4a38f970ad4af
Nir Yariv <niryariv@gmail.com>
2012-12-13 16:28:05 +0200

    show info box

---
+++ attrs.js
@@ -0,0 +1,56 @@
+var attrs = {
+	jerusalem : [
+		{ name: 'מבנה הרשו',	value: 3 },
+		{ name: 'שמות',			value: 3 },
+		{ name: 'טלפונים',		value: 3 },
+		{ name: 'דואל',			value: 3 },
+
+		{ name: 'שקיפות פרוצדוראלית',	value: null },
+		{ name: 'שעות קבלת קהל',	value: 3 },
+		{ name: 'מספרי טלפון',		value: 3 },
+		{ name: 'כתובת פיזית של משרדי העירייה',	value: 3 },
+
+		{ name: 'פרטי הממונה על חפש המידע',	value: 3 },
+		{ name: 'שם', value: 3 },
+
+		{ name: 'פרסום חוק חפש המידע', value: 3 },
+
+		{ name: 'פניה לחברי מועצה', value: 3 },
+		{ name: 'שמות', 	value: 3 },
+		{ name: 'טלפונים',	value: 3 },
+		{ name: 'דוא"ל',		value: 3 },
+
+		{ name: 'פרסום מוקדם של סדר יום ישיבות הרשות',	value: 3 },
+
+		{ name: 'פרוטוקולים מישיבות מועצה',	value: 3 },
+		{ name: 'צלום/הקלטת אודיו מהישיבה',	value: 3 },
+
+		{ name: 'דו"ח שנתי של מבקר העירייה',	value: 3 },
+
+		{ name: 'דו"ח שנתי לפי חוק חפש המידע',	value: 3 },
+
+		{ name: 'הצהרת נגודי ענינים של הנבחרים',	value: 0 },
+		{ name: 'ניגוד עניינים מופיע בפרוטוקולים',	value: 0 },
+
+		{ name: 'תקציב עירוני שנתי',	value: 3 },
+		{ name: 'איכות הפרסום',		value: 3 },
+		{ name: 'רלבנטיות הפרסום',	value: 3 },
+
+		{ name: 'פרוטוקולים ועדת תכנון ובניה',	value: 3 },
+
+		{ name: 'מכרזים עירוניים',		value: 3 },
+		{ name: 'קריטריונים במכרז',	value: 3 },
+
+		{ name: 'פרסום נתוני איכות הסביבה ',	value: 3 },
+
+		{ name: 'תכנית / קוד אתי',	value: 2 }
+	]
+};
+
+// dummy data - for sake of testing
+attrs['telaviv'] = attrs['jerusalem'];
+attrs['haifa'] = attrs['jerusalem'];
+attrs['beersheva'] = attrs['jerusalem'];
+attrs['ashkelon'] = attrs['jerusalem'];
+attrs['tiberia'] = attrs['jerusalem'];
+attrs['zefat'] = attrs['jerusalem'];
