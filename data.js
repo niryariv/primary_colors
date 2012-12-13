fdd28f0265d5c1fd95a9d27cc84800be3f3fe76b
Nir Yariv <niryariv@gmail.com>
2012-12-13 11:10:15 +0200

    add tiberia, zefat and ashkelon

--- data.js
+++ data.js
@@ -4,4 +4,7 @@ var data = [
 	{ id: "haifa", 		name: "חיפה",		coords: [32.81, 34.98], score: 86 	},
 	{ id: "jerusalem",	name: "ירושלים",		coords: [31.78, 35.21], score: 91 	},
 	{ id: "beersheva",	name: "באר שבע",	coords: [31.24, 34.79], score: 80 	},
+	{ id: "ashkelon",	name: "אשקלון",		coords: [31.71, 34.58], score: 70 	},
+	{ id: "tiberia",	name: "טבריה",		coords: [32.81, 35.52], score: 49.5	},
+	{ id: "zefat",		name: "צפת",		coords: [32.97, 35.50], score: 26	},
 ]
