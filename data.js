493c3cb6d25ef02f691b0a48b2f9eedcd78df9f7
Nir Yariv <niryariv@gmail.com>
2012-12-13 10:41:17 +0200

    show city outline

--- data.js
+++ data.js
@@ -1,7 +1,7 @@
 
 var data = [
-	{ name: "תל אביב",	coords: [32.06, 34.78], score: 83.5 },
-	{ name: "ירושלים",	coords: [31.78, 35.21], score: 91 	},
-	{ name: "חיפה",		coords: [32.81, 34.98], score: 86 	},
-	{ name: "באר שבע",	coords: [31.24, 34.79], score: 80 	},
+	{ id: 1, name: "תל אביב",	coords: [32.06, 34.78], score: 83.5 },
+	{ id: 2, name: "חיפה",		coords: [32.81, 34.98], score: 86 	},
+	{ id: 3, name: "ירושלים",		coords: [31.78, 35.21], score: 91 	},
+	{ id: 4, name: "באר שבע",	coords: [31.24, 34.79], score: 80 	},
 ]
