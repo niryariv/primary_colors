ab23e6534fa6e27ec29948c1b8b931c16137b9d4
Nir Yariv <niryariv@gmail.com>
2012-12-16 11:03:35 +0200

    rename to index.js

---
+++ data/index.js
@@ -0,0 +1,10 @@
+
+var data = [
+	{ id: "telaviv",	name: "תל אביב",		coords: [32.06, 34.78], score: 83.5 },
+	{ id: "haifa", 		name: "חיפה",		coords: [32.81, 34.98], score: 86 	},
+	{ id: "jerusalem",	name: "ירושלים",		coords: [31.78, 35.21], score: 91 	},
+	{ id: "beersheva",	name: "באר שבע",	coords: [31.24, 34.79], score: 80 	},
+	{ id: "ashkelon",	name: "אשקלון",		coords: [31.71, 34.58], score: 70 	},
+	{ id: "tiberia",	name: "טבריה",		coords: [32.81, 35.52], score: 49.5	},
+	{ id: "zefat",		name: "צפת",		coords: [32.97, 35.50], score: 26	},
+]
